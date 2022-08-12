1ae53d2844b01b132cbc5e5430e81712e6488f67
Matt <matt@multicode.co.uk>
2022-08-12 18:58:31 +0000

    latest

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +13,8 @@ public class Main {
         books.add(new Book(1,"Learning Java", "Matt"));
         books.add(new Book(2,"More Java", "Matt Again"));
         books.add(new Book(3,"Advanced Java", "A Different Matt"));
+
+        books.stream().forEach ( book -> book.setPrice(new BigDecimal("100"))   );
 
         ObjectMapper om = new ObjectMapper();
         System.out.println(om.writeValueAsString(books));
